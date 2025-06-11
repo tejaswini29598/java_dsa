d40815f8cff1a8d27808657bd2120d480aa83b63
tejaswini29598 <tejaswinimediboina@gmail.com>
2025-06-11 14:05:39 +0530

    assignments file added and notes updated

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        float num = 5.5f;
+        System.out.println(num);
+        float n = 5+5.5f;
+        System.out.println(n);
+    }
+}
